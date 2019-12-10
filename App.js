2c9c324fd8bb604a6044a36b36645c50fa7a627e
Aman Mittal <amandeepmittal@live.com>
2019-12-10 23:40:45 +0530

    add swipe views left and right

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ import {
   View,
   Text,
   StatusBar,
-  FlatList
+  FlatList,
+  Animated
 } from 'react-native'
+import Swipeable from 'react-native-gesture-handler/Swipeable'
 
 const mockDataList = [
   {
@@ -19,10 +21,67 @@ const mockDataList = [
 
 const Separator = () => <View style={styles.itemSeparator} />
 
+const LeftActions = (progress, dragX) => {
+  const scale = dragX.interpolate({
+    inputRange: [0, 100],
+    outputRange: [0, 1],
+    extrapolate: 'clamp'
+  })
+  return (
+    <View
+      style={{ flex: 1, backgroundColor: 'blue', justifyContent: 'center' }}>
+      <Animated.Text
+        style={{
+          color: 'white',
+          paddingHorizontal: 10,
+          fontWeight: '600',
+          transform: [{ scale }]
+        }}>
+        Left Action
+      </Animated.Text>
+    </View>
+  )
+}
+
+const RightActions = (progress, dragX) => {
+  const scale = dragX.interpolate({
+    inputRange: [-100, 0],
+    outputRange: [0.7, 0]
+  })
+  return (
+    <>
+      <View style={{ backgroundColor: 'red', justifyContent: 'center' }}>
+        <Animated.Text
+          style={{
+            color: 'white',
+            paddingHorizontal: 10,
+            fontWeight: '600',
+            transform: [{ scale }]
+          }}>
+          Delete
+        </Animated.Text>
+      </View>
+      <View style={{ backgroundColor: 'green', justifyContent: 'center' }}>
+        <Animated.Text
+          style={{
+            color: 'white',
+            paddingHorizontal: 10,
+            fontWeight: '600',
+            transform: [{ scale }]
+          }}>
+          Archive
+        </Animated.Text>
+      </View>
+    </>
+  )
+}
+
 const ListItem = ({ text }) => (
-  <View style={{ paddingVertical: 20 }}>
-    <Text style={{ fontSize: 24 }}>{text}</Text>
-  </View>
+  <Swipeable renderLeftActions={LeftActions} renderRightActions={RightActions}>
+    <View style={{ paddingVertical: 20 }}>
+      <Text style={{ fontSize: 24, paddingHorizontal: 10 }}>{text}</Text>
+    </View>
+  </Swipeable>
 )
 
 const App = () => {
@@ -30,14 +89,12 @@ const App = () => {
     <>
       <StatusBar barStyle='dark-content' />
       <SafeAreaView style={styles.container}>
-        <View style={styles.innerContainer}>
-          <FlatList
-            data={mockDataList}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => <ListItem {...item} />}
-            ItemSeparatorComponent={() => <Separator />}
-          />
-        </View>
+        <FlatList
+          data={mockDataList}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <ListItem {...item} />}
+          ItemSeparatorComponent={() => <Separator />}
+        />
       </SafeAreaView>
     </>
   )
@@ -46,10 +103,6 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1
-  },
-  innerContainer: {
-    flex: 1,
-    paddingHorizontal: 20
   },
   itemSeparator: {
     flex: 1,
